59e74be1c0aedc6e2cf8b8ca2aedfd0198ba032c
SofiaTilde <sofia.tilde@gmail.com>
2017-06-09 14:37:32 +0200

    Add files via upload

---
+++ Tiles/Gemstones/SapphirePlant.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.Enums;
+
+namespace ExtendedCrops.Tiles.Gemstones
+{
+	public class SapphirePlant : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoFail[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.StyleAlch);
+		}
+
+		public override bool Drop(int i, int j)
+		{
+			Random random = new Random();
+			int dust = random.Next(1, 10);
+			int seeds = random.Next(2, 5);
+
+			int stage = Main.tile[i, j].frameX / 18;
+			if (stage == 0)
+			{
+				Item.NewItem(i * 16, j * 16, 0, 0, mod.ItemType<Items.Gemstones.Seeds.SapphireSeeds>(), 1);
+			}
+			if (stage == 1)
+			{
+				Item.NewItem(i * 16, j * 16, 0, 0, mod.ItemType<Items.Gemstones.Seeds.SapphireSeeds>(), 1);
+			}
+			if (stage == 2)
+			{
+				Item.NewItem(i * 16, j * 16, 0, 0, mod.ItemType<Items.Gemstones.Seeds.SapphireSeeds>(), seeds);
+				Item.NewItem(i * 16, j * 16, 0, 0, mod.ItemType<Items.Gemstones.Dusts.SapphireDust>(), dust);
+			}
+			return false;
+		}
+
+		public override void RandomUpdate(int i, int j)
+		{
+			if (Main.tile[i, j].frameX == 0)
+			{
+				Main.tile[i, j].frameX += 18;
+			}
+			else if (Main.tile[i, j].frameX == 18)
+			{
+				Main.tile[i, j].frameX += 18;
+			}
+		}
+	}
+}
