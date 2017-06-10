7dfea0b26450027728c859f18105b37c07c79042
SofiaTilde <sofia.tilde@gmail.com>
2017-06-10 13:38:58 +0200

    Create CSilverSeedProcessor.cs

--- Tiles/Processors/CSilverSeedProcessor.cs
+++ Tiles/Processors/CSilverSeedProcessor.cs
@@ -18,7 +18,9 @@ namespace ExtendedCrops.Tiles.Processors
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
 			TileObjectData.addTile(Type);
-			AddMapEntry(new Color(200, 200, 200), "Silver Seed Processor");
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Silver Seed Processor");
+			AddMapEntry(new Color(200, 200, 200), name);
 			disableSmartCursor = true;
 		}
 
