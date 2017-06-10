54f2ad0deebdc522d8bed0c0dc04e5267bb39130
SofiaTilde <sofia.tilde@gmail.com>
2017-06-10 13:48:17 +0200

    Create LHallowedSeedProcessor.cs

--- Tiles/Processors/LHallowedSeedProcessor.cs
+++ Tiles/Processors/LHallowedSeedProcessor.cs
@@ -18,7 +18,9 @@ namespace ExtendedCrops.Tiles.Processors
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
 			TileObjectData.addTile(Type);
-			AddMapEntry(new Color(200, 200, 200), "Hallowed Seed Processor");
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Hallowed Seed Processor");
+			AddMapEntry(new Color(200, 200, 200), name);
 			disableSmartCursor = true;
 		}
 
