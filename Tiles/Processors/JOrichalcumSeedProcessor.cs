0c8aac58fc4465885c34d3ecff4c0666b0cfb3ab
SofiaTilde <sofia.tilde@gmail.com>
2017-06-10 13:46:46 +0200

    Create JOrichalcumSeedProcessor.cs

--- Tiles/Processors/JOrichalcumSeedProcessor.cs
+++ Tiles/Processors/JOrichalcumSeedProcessor.cs
@@ -18,7 +18,9 @@ namespace ExtendedCrops.Tiles.Processors
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
 			TileObjectData.addTile(Type);
-			AddMapEntry(new Color(200, 200, 200), "Orichalcum Seed Processor");
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Orichalcum Seed Processor");
+			AddMapEntry(new Color(200, 200, 200), name);
 			disableSmartCursor = true;
 		}
 
