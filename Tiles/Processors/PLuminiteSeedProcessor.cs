e31d5c309c4133ae2a8ea8fbe462958ae4c4258d
SofiaTilde <sofia.tilde@gmail.com>
2017-06-10 13:50:08 +0200

    Create PLuminiteSeedProcessor.cs

--- Tiles/Processors/PLuminiteSeedProcessor.cs
+++ Tiles/Processors/PLuminiteSeedProcessor.cs
@@ -18,7 +18,9 @@ namespace ExtendedCrops.Tiles.Processors
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
 			TileObjectData.addTile(Type);
-			AddMapEntry(new Color(200, 200, 200), "Luminite Seed Processor");
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Luminite Seed Processor");
+			AddMapEntry(new Color(200, 200, 200), name);
 			disableSmartCursor = true;
 		}
 
