0cc58779ca70369c011f2655fbcd4e345f8c2bb4
SofiaTilde <sofia.tilde@gmail.com>
2017-06-10 13:37:47 +0200

    Create ATinSeedProcessor.cs

--- Tiles/Processors/ATinSeedProcessor.cs
+++ Tiles/Processors/ATinSeedProcessor.cs
@@ -18,7 +18,9 @@ namespace ExtendedCrops.Tiles.Processors
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
 			TileObjectData.newTile.CoordinateHeights = new int[]{ 18 };
 			TileObjectData.addTile(Type);
-			AddMapEntry(new Color(200, 200, 200), "Tin Seed Processor");
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Tin Seed Processor");
+			AddMapEntry(new Color(200, 200, 200), name);
 			disableSmartCursor = true;
 		}
 
